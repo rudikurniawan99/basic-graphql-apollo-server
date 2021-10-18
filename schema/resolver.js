a93c088a3bdc4f07f8bb4a84e93eaa58e0867df9
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-10-18 16:46:26 +0700

    create feature typedef and resolver for user to query and create user data

--- schema/resolver.js
+++ schema/resolver.js
@@ -1,9 +1,25 @@
-
-
+const { users } = require('../dummy-data/dummy')
+const { v4: uuidv4 } = require('uuid')
 
 const resolvers = {
   Query: {
+    users: () => users 
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const { name, username, email, photoProfile } = args.input
+      const user = {
+        id: uuidv4(),
+        name,
+        username,
+        email,
+        photoProfile
+      }
 
+      users.push(user)
+      console.log(users)
+      return user
+    }
   }
 }
 
--- schema/typedef.js
+++ schema/typedef.js
@@ -1,7 +1,28 @@
-const { sql } = require('apollo-server')
+const { gql } = require('apollo-server')
 
-const typeDefs = sql `
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+    email: String!
+    photoProfile: String 
+  }
 
+  input CreateUserInput {
+    name: String!
+    username: String!
+    email: String!
+    photoProfile: String
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput): User
+  }
+
+  type Query {
+    users: [User]
+  }
 `
 
 module.exports = { typeDefs }
